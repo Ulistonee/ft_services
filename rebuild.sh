4aae1636d042c6f40ce4bcd857a91bac718e6a78
Ulistonee <ulistonee@gmail.com>
2021-04-29 22:54:30 +0300

    php not working

--- rebuild.sh
+++ rebuild.sh
@@ -4,8 +4,12 @@ eval $(minikube docker-env)
 
 kubectl delete svc nginx-svc
 kubectl delete deploy nginx-deployment
+kubectl delete svc phpmyadmin-svc
+kubectl delete deploy phpmyadmin-deployment
 
 docker build -t nginx_image srcs/nginx
+docker build -t phpmyadmin_image srcs/phpmyadmin/
 
 kubectl apply -f srcs/nginx/srcs/nginx.yaml
 kubectl apply -f srcs/nginx/srcs/configmap.yaml
+kubectl apply -f srcs/phpmyadmin/srcs/phpmyadmin.yaml
--- setup.sh
+++ setup.sh
@@ -4,5 +4,17 @@ minikube start --vm-driver=virtualbox
 minikube addons enable metallb
 eval $(minikube docker-env)
 docker build -t nginx_image srcs/nginx
-kubectl apply -f srcs/nginx/srcs/nginx.yaml
+eval $(minikube docker-env)
+docker build -t phpmyadmin_image srcs/phpmyadmin/
+
 kubectl apply -f srcs/nginx/srcs/configmap.yaml
+kubectl apply -f srcs/nginx/srcs/nginx.yaml
+kubectl apply -f srcs/phpmyadmin/srcs/phpmyadmin.yaml
+
+
+# kubectl get pods --all-namespaces       мы можем увидеть список запущенных в кластере подов
+# kubectl get nodes                       список нод
+# kubectl get services                    список сервисов
+# minikube service hello-minikube --url   команда, чтобы узнать его внешний IP и порт
+# kubectl delete service
+# kubectl delete deployment
